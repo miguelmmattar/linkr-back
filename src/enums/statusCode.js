5b375db998f757a5b013d36935c415810968dae5
Allan Vieira Coelho <github@allanvieira.dev>
2022-10-19 16:21:22 -0300

    feat(middleware/HTMLSanitizer): HTML sanitization has been implemented

---
+++ src/enums/statusCode.js
@@ -0,0 +1,15 @@
+const STATUS_CODE = Object.freeze({
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    CONFLICT: 409,
+    UNPROCESSABLE_ENTITY: 422,
+    SERVER_ERROR: 500,
+  });
+  
+  export { STATUS_CODE };
+  
