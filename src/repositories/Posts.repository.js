2bf163b2dd6019abf072625fcb3ed70ce377dd6b
Miguel Faria <mattar1704miguel@gmail.com>
2022-10-19 21:50:27 -0300

    small changes

--- src/repositories/Posts.repository.js
+++ src/repositories/Posts.repository.js
@@ -1,19 +1,17 @@
 import connection from "../database/Postgres.js";
 
-const postUrl = async (url, description, user) => {
+const postUrl = (url, description, user) => {
     return connection.query(`
         INSERT INTO posts (url, description, "userId") VALUES ($1, $2, $3)
     `, [url, description, user]);
 }
 
-const getPosts = async () => {
-    const posts = connection.query(`
+const getPosts = () => {
+    return connection.query(`
         SELECT url, description, "userId", "createdAt" FROM posts
             ORDER BY "createdAt" DESC
             LIMIT 20;
     `);
-
-    return posts.rows;
 }
 
 export {
