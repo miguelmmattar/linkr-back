ab5273825a1edf67dfb12d8eca2ec82008429db3
Allan Vieira Coelho <github@allanvieira.dev>
2022-10-22 18:11:27 -0300

    refactor[postLike]: some changes to enhance code performance

--- src/controllers/Likes.controller.js
+++ src/controllers/Likes.controller.js
@@ -3,14 +3,18 @@ import * as likesRepository from "../repositories/Likes.repository.js";
 async function postLike(request, response) {
   try {
     const { userId } = response.locals;
-    const { postId } = response.locals.body;
-    const deletePreviousLike = likesRepository.deleteLike({ userId, postId });
-    const query = await likesRepository.insertLike({ userId, postId });
+    const { id } = response.locals.safeData;
+    const deletePreviousLike = await likesRepository.deleteLike({
+      userId,
+      postId: id,
+    });
 
     if (deletePreviousLike.rowCount !== 0) {
       response.sendStatus(STATUS_CODE.NO_CONTENT);
       return;
     }
+
+    const query = await likesRepository.insertLike({ userId, postId: id });
 
     if (query.rowCount === 0) {
       response
